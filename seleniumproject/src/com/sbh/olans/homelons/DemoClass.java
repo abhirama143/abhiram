61128be7d3de07fff642c71fa970036ae0265208
prafulvenkat <venkatpraful@gmail.com>
2018-05-29 11:33:00 +0530

    added message

---
+++ seleniumproject/src/com/sbh/olans/homelons/DemoClass.java
@@ -0,0 +1,10 @@
+package com.sbh.olans.homelons;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+	}
+
+}
